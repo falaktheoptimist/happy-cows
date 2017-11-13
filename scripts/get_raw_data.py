e17f36efab0d1e46722e0112c9c1058e826532f6
Brandon Beidel <bcbeidel@users.noreply.github.com>
2017-11-13 11:14:49 -0500

    Script Cleanup for Downloading Raw Data from AWS (#4)
    * changes for modularity
    
    * linting

--- scripts/get_raw_data.py
+++ scripts/get_raw_data.py
@@ -1,37 +1,52 @@
+# -*- coding: utf-8 -*-
+"""Raw Data Retrieval
+
+This script populates the ./data/raw folders with an raw files currently stored on AWS S3.
+
+Proper configuration and placement of .config.yml in the root directory is required for operation.
+"""
+import sys
+import os
 import boto3  # AWS python SDK
 import yaml   # yaml file interpretation
-import sys, os
 
-# Load config
-with open(".config.yml", 'r') as ymlfile:
-    config = yaml.load(ymlfile)
+def get_config():
+    """Returns Configuration Dictionary"""
+    with open(".config.yml", 'r') as ymlfile:
+        config = yaml.load(ymlfile)
+    return config
 
-datasets = config['datasets']
-s3 = boto3.resource(
-    's3',
-    aws_access_key_id=config['aws']['access_key'],
-    aws_secret_access_key=config['aws']['secret_key']
-)
+def ensure_directory_exists(path):
+    """Validates that a directory exists.  Creates directory if it does not."""
+    if not os.path.isdir(path):
+        os.makedirs(path)
 
-def CheckDirectory(relative_path):
-    if not os.path.isdir(relative_path):
-        os.makedirs(relative_path)
-
-def DownloadDataset(dataset, s3):
-    current_bucket = s3.Bucket(dataset['bucket-name']) 
-    bucket_list = current_bucket.objects.all()
-    
-    CheckDirectory(dataset['local-target-folder'])
+def download_dataset(target_directory, bucket_name, s3):
+    """Downloads an S3 Dataset and places it in a targeted directory"""
+    ensure_directory_exists(target_directory)
+    bucket_list = s3.Bucket(bucket_name).objects.all()
 
     for bucket_file in bucket_list:
-        origin = bucket_file.key
-        target = dataset['local-target-folder'] + bucket_file.key
-        print(f'|---> {origin}')
-        s3.Bucket(dataset['bucket-name']).download_file(origin, target)
+        target = target_directory + bucket_file.key
+        print(f'|---> {bucket_file.key}')
+        s3.Bucket(bucket_name).download_file(bucket_file.key, target)
 
-for dataset in datasets:
-    print(f'Download of {dataset["name"]} data starting...')
-    DownloadDataset(dataset, s3)
-    print(f'Download of {dataset["name"]} data complete...')
+def main():
+    """Gets raw data from AWS"""
+    cfg = get_config()
+    s3_resource = boto3.resource(
+        's3',
+        aws_access_key_id=cfg['aws']['access_key'],
+        aws_secret_access_key=cfg['aws']['secret_key']
+    )
 
-print("Raw data has been successfully downloaded and can be found in the /data/raw/ directory.")
+    for dataset in cfg['datasets']:
+        download_dataset(
+            target_directory=dataset['local-target-folder'],
+            bucket_name=dataset['bucket-name'],
+            s3=s3_resource
+        )
+    print("Download Complete.")
+
+if __name__ == "__main__":
+    sys.exit(main())
