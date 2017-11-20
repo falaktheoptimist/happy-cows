e7b05c20a092d6f1cadeeb435e9ffe29cc866506
bcbeidel <bcbeidel@gmail.com>
2017-11-19 21:12:29 -0500

    update weather parsing script

--- scripts/parse_weather.py
+++ scripts/parse_weather.py
@@ -1,7 +1,25 @@
 """
 Methods to parse weather data from csv files and return a dataframe
 """
+import sys
+import glob
+import random
 import pandas as pd
+import helper_functions as helper
+from datetime import datetime
 
 def get_dataframe_from_file(data_file):
-    return pd.read_csv(data_file,sep=',',header=0)
+    weather = pd.read_csv(data_file,sep=',', header=0)[[
+        'STATION', 'NAME', 'LATITUDE', 'LONGITUDE', 'ELEVATION', 'DATE', 'PRCP', 'TMIN', 'TMAX'
+    ]]
+    weather['DATE'] = weather['DATE'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    weather.index = weather['DATE']
+    return weather
+
+def main():
+    """ Selects a random weather file, parses file, prints df.info()"""
+    files = glob.glob(f"{helper.get_config()['datasets']['weather']['regex']}")
+    print(get_dataframe_from_file(random.choice(files)).info())
+
+if __name__ == '__main__':
+    sys.exit(main())
