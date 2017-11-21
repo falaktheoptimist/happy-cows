015624f41fb4839fcf44fa96a068242f1a05dc6d
Brandon Beidel <bcbeidel@users.noreply.github.com>
2017-11-20 20:14:00 -0500

    Data Wrangling Writeup & Initial Python Notebook (#13)
    * median and mean milk production per cow
    
    * update weather parsing script
    
    * additional content for description of weather data wrangling
    
    * successfuly queries of database into notebook
    
    * docstring, and reorder imports
    
    * example files, and data wrangling for milk volume
    
    * spelling, classificaiton data wrangling

--- scripts/parse_weather.py
+++ scripts/parse_weather.py
@@ -1,7 +1,41 @@
 """
 Methods to parse weather data from csv files and return a dataframe
 """
+import sys
+import glob
+import random
+from datetime import datetime
 import pandas as pd
+import pandas.api.types as ptypes
+import helper_functions as helper
 
 def get_dataframe_from_file(data_file):
-    return pd.read_csv(data_file,sep=',',header=0)
+    """ Parses NOAA daily weather summary CSVs. Returns DataFrame """
+    weather = pd.read_csv(data_file, sep=',', header=0)[[
+        'STATION', 'NAME', 'LATITUDE', 'LONGITUDE', 'ELEVATION', 'DATE', 'PRCP', 'TMIN', 'TMAX'
+    ]]
+    weather['DATE'] = weather['DATE'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    weather.set_index(['DATE', 'STATION'])
+    
+    assert weather[weather['TMAX'].notnull()]['TMAX'].between(-20, 120).all()
+    assert weather[weather['TMIN'].notnull()]['TMIN'].between(-20, 120).all()
+    assert weather[weather['PRCP'].notnull()]['PRCP'].between(0, 20).all()
+    assert ptypes.is_string_dtype(weather['STATION'])
+    assert ptypes.is_string_dtype(weather['NAME'])
+    assert ptypes.is_datetime64_any_dtype(weather['DATE'])
+    assert ptypes.is_numeric_dtype(weather['LATITUDE'])
+    assert ptypes.is_numeric_dtype(weather['LONGITUDE'])
+    assert ptypes.is_numeric_dtype(weather['ELEVATION'])
+    assert ptypes.is_numeric_dtype(weather['PRCP'])
+    assert ptypes.is_numeric_dtype(weather['TMIN'])
+    assert ptypes.is_numeric_dtype(weather['TMAX'])
+
+    return weather
+
+def main():
+    """ Selects a random weather file, parses file, prints df.info()"""
+    files = glob.glob(f"{helper.get_config()['datasets']['weather']['regex']}")
+    print(get_dataframe_from_file(random.choice(files)).info())
+
+if __name__ == '__main__':
+    sys.exit(main())
