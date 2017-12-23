c72b330d5ebcdd33846ab299e27b82d8628bd7e3
bcbeidel <bcbeidel@gmail.com>
2017-12-22 21:19:58 -0500

    change handlnig of days since calvings, updated reports

--- scripts/add_days_since_calving.py
+++ scripts/add_days_since_calving.py
@@ -8,19 +8,20 @@ import helper_functions as helper
 
 def retrieve_data(connection):
     milk = pd.read_sql_table('milk_volume', connection)
-    score = pd.read_sql_table('classification', connection)
-    return milk, score
+    calvings = pd.read_sql_table('calvings', connection)
+    return milk, calvings
 
 def get_calving_dates(classification):
-    return classification[['animal_id', 'date_calved']].dropna(how='any').sort_values('date_calved')
+    return classification[['animal_id', 'calving_date']].dropna(how='any').sort_values('date_calved')
 
 def add_days_since_calving(milk, calvings):
     calving_dates = []
     for index, row in milk.iterrows():
         milk_date = row['date']
         animal_id = row['animal_id']
-        c = calvings[(calvings['date_calved'] <= milk_date) & (calvings['animal_id'] == animal_id)]
-        c = c['date_calved']
+        c = calvings[(calvings['calving_date'] <= milk_date) & (calvings['animal_id'] == animal_id)]
+        c = c['calving_date']
+        print(c)
         if len(c) == 0:
             calving_dates.append(np.NaN)
         else:
@@ -35,8 +36,7 @@ def add_days_since_calving(milk, calvings):
 def main():
     """ Loads add days-since-calving to milk data """
     with helper.get_db_engine().connect() as con:
-        milk, score = retrieve_data(con)
-        calvings = get_calving_dates(score)
+        milk, calvings = retrieve_data(con)
         milk = add_days_since_calving(milk, calvings)
         milk.to_sql(con=con, name='milk_volume', if_exists='replace')
 
